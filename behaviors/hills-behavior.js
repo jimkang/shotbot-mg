efb98807056db2135b7eb8bb32843381da163c98
Jim Kang <jimkang@gmail.com>
2020-08-09 18:16:19 -0400

    Make hills still most of the time. In those cases, make pngs, not gifs.

--- behaviors/hills-behavior.js
+++ behaviors/hills-behavior.js
@@ -1,33 +1,29 @@
 var callNextTick = require('call-next-tick');
 var { Tablenest, d } = require('tablenest');
+var probable = require('probable');
 
-var tablenest = Tablenest();
+var tablenest = Tablenest({ probable });
 
 var tweenLengthTableRoll = tablenest({
   root: [[1, d`d1000+1000`], [4, d`d5000+2000`], [2, d`d23000+7000`]]
 });
 
-function generateImageURL(done) {
+function generate(done) {
   const seed = new Date().toISOString();
   const tweenLengthMS = tweenLengthTableRoll();
-  const url = `https://jimkang.com/hills/#tweenBetweenPairs=yes&tweenLengthMS=${tweenLengthMS}&seed=${seed}`;
+  const shouldAnimate = probable.roll(3) === 0;
+  var url = `https://jimkang.com/hills/#seed=${seed}`;
+  if (shouldAnimate) {
+    url += `&tweenBetweenPairs=yes&tweenLengthMS=${tweenLengthMS}`;
+  }
+
   const caption = `<a href="${url}">High-def version</a>`;
 
-  callNextTick(done, null, {
-    url,
-    altText: 'Hills!',
-    caption
-  });
-}
-
-module.exports = {
-  postingTargets: ['archive'],
-  generateImageURL,
-  webimageOpts: {
+  var webimageOpts = {
     screenshotOpts: {
       clip: {
-        x: 96,
-        y: 23,
+        x: 98,
+        y: 16,
         width: 804,
         height: 454
       }
@@ -36,11 +32,28 @@ module.exports = {
       width: 1000,
       height: 563,
       deviceScaleFactor: 1
-    },
-    burstCount: 48,
-    timeBetweenBursts: 1000 / 12,
-    makeBurstsIntoAnimatedGif: true
-  },
+    }
+  };
+
+  if (shouldAnimate) {
+    Object.assign(webimageOpts, {
+      burstCount: 48,
+      timeBetweenBursts: 1000 / 12,
+      makeBurstsIntoAnimatedGif: true
+    });
+  }
+
+  callNextTick(done, null, {
+    url,
+    altText: 'Hills!',
+    caption,
+    webimageOpts
+  });
+}
+
+module.exports = {
+  postingTargets: ['archive'],
+  generate,
   archive: {
     name: 'Hills',
     title: 'Hills',
